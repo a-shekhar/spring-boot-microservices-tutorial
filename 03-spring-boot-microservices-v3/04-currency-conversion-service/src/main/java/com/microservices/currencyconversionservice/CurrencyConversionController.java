530f44fb53f310f0e0017d288e16dfa69642900e
a-shekhar <rajadityasingh31@gmail.com>
2023-07-21 10:14:29 +0530

    Feign REST Client

--- 03-spring-boot-microservices-v3/04-currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversionController.java
+++ 03-spring-boot-microservices-v3/04-currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,5 +1,6 @@
 package com.microservices.currencyconversionservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,9 @@ import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
+
+    @Autowired
+    private CurrencyExchangeProxy proxy;
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public  CurrencyConversion calculateCurrencyConversion(
@@ -31,6 +35,32 @@ public class CurrencyConversionController {
         CurrencyConversion currencyConversion = responseEntity.getBody();
 
         return new CurrencyConversion(currencyConversion.getId(), currencyConversion.getFrom(), currencyConversion.getTo(), quantity,
-                currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment());
+                currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment() + " " + "rest template");
+    }
+
+
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public  CurrencyConversion calculateCurrencyConversionFeign(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+    ){
+
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+
+        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate()
+                .getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                        CurrencyConversion.class, uriVariables);
+
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+
+        return new CurrencyConversion(currencyConversion.getId(), currencyConversion.getFrom(), currencyConversion.getTo(), quantity,
+                currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment() + " " + "feign");
     }
 }
